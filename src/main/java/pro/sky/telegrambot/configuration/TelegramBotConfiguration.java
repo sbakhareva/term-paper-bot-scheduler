d267f5d7d4999ac5e77f0352633b583a6939ff30
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-04-30 12:18:41 +0300

    создана сущность notificationtask

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,7 +1,7 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.DeleteMyCommands;
+import com.pengrad.telegrambot.request.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -4,12 +4,12 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
