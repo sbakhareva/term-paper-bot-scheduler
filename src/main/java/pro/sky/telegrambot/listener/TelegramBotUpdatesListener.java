bb4a74dda828c25d263ccf41b53135b260f06614
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-04-30 11:02:09 +0300

    бот научился говорить "привет"

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +25,24 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
+    private void sendWelcomeMessage(long chatId) {
+        String welcomeText = "Привет!";
+        SendMessage request = new SendMessage(chatId, welcomeText);
+        telegramBot.execute(request);
+    }
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message() != null && update.message().text() != null) {
+                String text = update.message().text();
+                long chatId = update.message().chat().id();
+
+                if ("/start".equals(text)) {
+                    sendWelcomeMessage(chatId);
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
